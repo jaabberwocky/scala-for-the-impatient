81c2da52c196630d25bcec114c62b9ec05282b97
tobias <leongtobias@gmail.com>
2021-04-11 14:31:08 +0800

    Add chapter code

--- src/main/scala/chapters/ch02_controlStructuresAndFunctions.scala
+++ src/main/scala/chapters/ch02_controlStructuresAndFunctions.scala
@@ -1,7 +1,34 @@
 package main.scala.chapters
 
+import scala.io.StdIn
+
 object ch02_controlStructuresAndFunctions extends App {
+
+  // ask for input
+  def printUserName(): Unit = {
+    val name = StdIn.readLine("Your name is: ")
+    print("Your age: ")
+    val age = StdIn.readInt()
+    println(s"Hello $name! You are $age years old today.")
+  }
+
+  // printUserName()
+
+  // while loop
+  println("While loop")
+  var n = 0
+  while (n < 10) {
+    println(n)
+    n += 1
+  }
+
+  // for loop
+  println("For loop")
+  for (i <- 1 to 10) {
+    println(i)
+  }
 
 
 
 }
+
